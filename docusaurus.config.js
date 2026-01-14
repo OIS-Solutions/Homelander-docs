48c17cc45d07785d622813f57b3c81ceb69421fe
Alexey Smirnov <alexey.smirnov@solidwall.io>
2026-01-14 14:14:30 +0300

    Add initial Homelander docs version

---
+++ docusaurus.config.js
@@ -0,0 +1,54 @@
+// @ts-check
+const config = {
+  title: 'MEV-X Homelander',
+  tagline: 'Post-swap, atomic MEV internalization for DEXs',
+  url: 'https://ois-solutions.github.io',
+  baseUrl: '/Homelander-docs/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.svg',
+  organizationName: 'OIS-Solutions',
+  projectName: 'Homelander-docs',
+  trailingSlash: true,
+  i18n: { defaultLocale: 'en', locales: ['en'] },
+  presets: [
+    [
+      'classic',
+      ({
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: require.resolve('./sidebars.js'),
+          breadcrumbs: true,
+        },
+        blog: false,
+        pages: false,
+        theme: { customCss: require.resolve('./src/css/custom.css') },
+      }),
+    ],
+  ],
+  themeConfig: ({
+    colorMode: {
+      defaultMode: 'dark',
+      respectPrefersColorScheme: true,
+      disableSwitch: false,
+    },
+  navbar: {
+  title: 'MEV-X Homelander',
+  logo: {
+    alt: 'MEV-X',
+    src: 'img/logo.png',
+  },
+  items: [
+  
+    { href: 'https://www.mev-x.com/', label: 'Website', position: 'right' },
+
+    { href: 'https://x.com/MEV_X_project', label: 'X', position: 'right' },
+
+    { href: 'https://github.com/OIS-Solutions/Homelander-docs', label: 'GitHub', position: 'right' },
+  ],
+},
+    prism: { additionalLanguages: ['solidity'] },
+    docs: { sidebar: { hideable: false } },
+  }),
+};
+module.exports = config;
---
+++ sidebars.js
@@ -0,0 +1,13 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  mainSidebar: [
+    'index',
+    'executive-summary',
+    'architecture-overview',
+    'integration-for-exchanges',
+    'profit-distribution-model',
+    'security-guarantees',
+    'audit',
+  ],
+};
+module.exports = sidebars;
